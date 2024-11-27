6fbe6359fdc7e7d344b51ba6165a3aa5cb61c9a5
halgendy <156970407+halgendy@users.noreply.github.com>
2024-11-27 18:32:20 -0500

    fixed movement

--- script/player/player.gd
+++ script/player/player.gd
@@ -9,9 +9,10 @@ var default_mouse_mode = Input.MOUSE_MODE_CAPTURED
 var pause_mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 # Player movement parameters
-const walk_acceleration: float = 14 # meters / second^2
-var player_damp: float = 14 # meters / second^2
+const walk_acceleration: float = 50 # meters / second^2
 const jump_velocity: float = 4.5 # meters / second
+const walk_velocity: float = 0.5 # meters / second
+const sprint_factor: float = 2 
 
 var mouse_sensitivity := 0.001
 var min_pitch := -60
@@ -60,6 +61,7 @@ func _ready() -> void:
 
 # Called every frame. (delta: float) is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	var walk_force = Vector3.ZERO
 	if can_move:
 		# Get direction player wants to move in based on input
 		# For get_axis: returns -1.0 if first, +1.0 if second, 0.0 if none OR both
@@ -70,43 +72,42 @@ func _process(_delta: float) -> void:
 		# Calculate movement direction relative to twist_pivot | normalized to fix diagonal
 		# Use the mass and acceleration to calculate force | F = ma
 		var directional_vector: Vector3 = twist_pivot.basis * cardinal_direction.normalized()
-		var move_force: float = (player_mass * walk_acceleration)
-		
-		var walk_force = directional_vector * move_force
-		if sprinting: walk_force *= 2
-		
-		# damp * walk_speed = move_force | damp = move_force/walk_speed
-		var damp_force = Vector3(linear_velocity.x, 0, linear_velocity.z) * player_damp 
-		
-		# For future reference, apply_central force based on time already, so no * delta
-		apply_central_force(walk_force - damp_force)
-		
-		# Debug velocity
-		#print("CURR_VELOCITY: ", self.linear_velocity.length())
-		
-		# Pause locking the mouse
-		if Input.is_action_just_pressed("ui_cancel"):
-			Input.set_mouse_mode(pause_mouse_mode)
-		elif Input.is_action_just_released("ui_cancel"):
-			Input.set_mouse_mode(default_mouse_mode)
-		# Apply a one-time impulse for jump force
-		elif Input.is_action_just_pressed("jump") and is_on_floor():
-			var jump_force = (Vector3.UP * jump_velocity * player_mass)
-			apply_central_impulse(jump_force)
-		# Check if player is sprinting
-		elif Input.is_action_just_pressed("sprint"):
-			sprinting = true
-		elif Input.is_action_just_released("sprint"):
-			sprinting = false
-		
-		# Rotate charcter based on mouse motion detected previously
-		twist_pivot.rotate_y(twist_input)
-		pitch_pivot.rotate_x(pitch_input)
-		pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, 
-			deg_to_rad(min_pitch),
-			deg_to_rad(max_pitch))
-		twist_input = 0.0
-		pitch_input = 0.0
+
+		walk_force = directional_vector * walk_acceleration
+		if sprinting: walk_force *= sprint_factor
+	
+	var damp_acceleration = walk_acceleration * walk_velocity
+	var damp_force = Vector3(linear_velocity.x, 0, linear_velocity.z) * damp_acceleration 
+	
+	# For future reference, apply_central force based on time already, so no * delta
+	apply_central_force((walk_force - damp_force) * player_mass)
+	
+	# Debug velocity
+	#print("CURR_VELOCITY: ", self.linear_velocity.length())
+	
+	# Pause locking the mouse
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(pause_mouse_mode)
+	elif Input.is_action_just_released("ui_cancel"):
+		Input.set_mouse_mode(default_mouse_mode)
+	# Apply a one-time impulse for jump force
+	elif Input.is_action_just_pressed("jump") and is_on_floor():
+		var jump_force = (Vector3.UP * jump_velocity * player_mass)
+		apply_central_impulse(jump_force)
+	# Check if player is sprinting
+	elif Input.is_action_just_pressed("sprint"):
+		sprinting = true
+	elif Input.is_action_just_released("sprint"):
+		sprinting = false
+	
+	# Rotate charcter based on mouse motion detected previously
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, 
+		deg_to_rad(min_pitch),
+		deg_to_rad(max_pitch))
+	twist_input = 0.0
+	pitch_input = 0.0
 	
 
 func _unhandled_input(event: InputEvent) -> void:
