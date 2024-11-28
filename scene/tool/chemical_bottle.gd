2b220b43eb9295a24e059f17539b637163dcd6ef
isaacspool <spool.i@northeastern.edu>
2024-11-27 19:55:17 -0500

    Fixed up tools and added them to alley level

---
+++ scene/tool/chemical_bottle.gd
@@ -0,0 +1,26 @@
+extends RigidBody3D
+
+@onready var bottle_body = $Bottle/Bottle_Body
+
+enum Chemical {
+	BLEACH,
+	ACID,
+	TOMATO_JUICE,
+	SOAPY_WATER
+}
+
+func get_chemical_color(chemical: Chemical):
+	var color = null
+	match chemical:
+		Chemical.BLEACH: color = Color("ddedfd")
+		Chemical.ACID: color = Color("90db81")
+		Chemical.TOMATO_JUICE: color = Color("a8212f")
+		Chemical.SOAPY_WATER: color = Color("fb9391")
+	return color
+
+
+
+@export var contents: Chemical
+
+func _ready():
+	bottle_body.material_override.albedo_color = get_chemical_color(contents)
