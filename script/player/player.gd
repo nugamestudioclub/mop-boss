940e89310c5e33093dcb8422e5e3b2f487097c71
halgendy <156970407+halgendy@users.noreply.github.com>
2024-12-05 21:49:21 -0500

    changed name of player_pov to camera for simplicity

--- script/player/player.gd
+++ script/player/player.gd
@@ -25,7 +25,7 @@ var pitch_input := 0.0
 # Basically waitForChild()
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
-@onready var camera := $TwistPivot/PitchPivot/PlayerPov
+@onready var camera := $TwistPivot/PitchPivot/Camera
 @onready var hand := $TwistPivot/PitchPivot/Hand
 @onready var inspect_inventory = $InspectInventory
 
